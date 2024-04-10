03b3e87445bd2f436669b2f43c1661666d1247a8
HanNilss <nilssonhannes36@gmail.com>
2024-04-10 13:39:38 -0400

    Initial commit, added gitPractice

---
+++ gitPractice.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @autor Hannes Nilsson
+ * 
+ */
+public class gitPractice {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hellor Dr. Johnson! This is Hannes Nilsson.");
+	}
+
+}
